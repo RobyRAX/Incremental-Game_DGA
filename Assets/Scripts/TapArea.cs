5d1ee36ee2f16c676ac778b1a41520aaf233b344
Robby Alexander <robyalexander571@gmail.com>
2022-02-04 00:12:45 +0700

    Initial Commit

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TapArea : MonoBehaviour
+{
+    //public Vector3 position;
+    public void UserTap()
+    {
+        //position.x = Input.mousePosition.x;
+        //position.y = Input.mousePosition.y;
+        GameManager.Instance.CollectByTap(Input.mousePosition, transform);
+        Debug.Log("Tes");
+    }
+}
